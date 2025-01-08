73cc0cd6bdbd5c7d8943dad01385d70530a36061
HoneySafonovets <kfs.boi@yandex.ru>
2025-01-09 01:29:56 +0300

    feat: add function canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -70,8 +70,18 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  if (queen.x === queen.y && king.y === king.x) {
+    return true;
+  }
+  if (king.x === 3 || king.y === 7) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
